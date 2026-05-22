bfdd2b4de8ac1969cd6c16f6aa518124dafb1363
whiteringogm <whiteringogm@gmail.com>
2026-05-22 22:33:28 +0900

    Bump cache after image preservation fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='imagegen-prompt-vault-v4';
+const CACHE_NAME='imagegen-prompt-vault-v5';
 const APP_SHELL=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
