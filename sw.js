4c8cb4f6505445adc1ea8693d6530aa2da8fdb51
whiteringogm <whiteringogm@gmail.com>
2026-05-22 22:01:55 +0900

    Bump cache for structured prompt app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='imagegen-prompt-vault-v2';
-const APP_SHELL=['./','./index.html','./manifest.json','./icon.svg'];
+const CACHE_NAME='imagegen-prompt-vault-v3';
+const APP_SHELL=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
   self.skipWaiting();
