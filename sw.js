f96f42807deb4be162cfe2100073664f4ceefd83
whiteringogm <whiteringogm@gmail.com>
2026-05-22 22:46:16 +0900

    Cache stable image display fixer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='imagegen-prompt-vault-v5';
-const APP_SHELL=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
+const CACHE_NAME='imagegen-prompt-vault-v6';
+const APP_SHELL=['./','./index.html','./styles.css','./app.js','./fix-img.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
   self.skipWaiting();
